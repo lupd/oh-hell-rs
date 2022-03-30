8efed13cd6e886c1ef016e33a77dd4c1ad997fe2
dlup <93457935+lupd@users.noreply.github.com>
2022-03-30 08:18:55 -0400

    Fix errors

--- oh_hell_rs/src/turn/mod.rs
+++ oh_hell_rs/src/turn/mod.rs
@@ -1,4 +1,4 @@
-use crate::{player::Player, card::Card};
+use crate::{card::Card, player::Player};
 
 /// Action by a player.
 #[derive(Clone, Copy, Eq, Hash, PartialEq)]
@@ -8,12 +8,19 @@ pub struct Turn {
 }
 
 impl Turn {
+    #[must_use]
+    pub const fn new(player: Player, card: Card) -> Self {
+        Self { player, card }
+    }
+
     /// Get the player who made this turn.
+    #[must_use]
     pub const fn player(&self) -> Player {
         self.player
     }
 
     /// Get the card played on this turn.
+    #[must_use]
     pub const fn card(&self) -> Card {
         self.card
     }
